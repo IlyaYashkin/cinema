3c5e857582ffcd188c00337e776bd26b97490883
ilyayashkin <iyashkin@ibs.ru>
2026-03-06 20:22:11 +0300

    replace interface{}

--- internal/lib/jwt/jwt.go
+++ internal/lib/jwt/jwt.go
@@ -80,7 +80,7 @@ func (g *Generator) GenerateRefreshToken(userId string) (string, error) {
 }
 
 func (g *Generator) ValidateToken(tokenString string) (*Claims, error) {
-	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (any, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
